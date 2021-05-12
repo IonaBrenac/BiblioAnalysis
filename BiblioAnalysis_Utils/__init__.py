7cb132c44daffe1fa259031240c17b46c81aa375
francois bertin <francois.bertin7@wanadoo.fr>
2021-05-12 09:49:27 +0200

    Split the BiblioParer.py module in three modules: BiblioParsingWos.py, BiblioParsingScopus.py, BiblioParsingUtils.py. Modification of the cell Parsing of BiblioAnalysis_OnDev.ipynb

--- BiblioAnalysis_Utils/__init__.py
+++ BiblioAnalysis_Utils/__init__.py
@@ -3,7 +3,9 @@ from .BiblioCoupling import *
 from .BiblioDescription import *
 from .BiblioFilter import *
 from .BiblioGeneralGlobals import *
-from .BiblioParser import *
+from .BiblioParsingScopus import *
+from .BiblioParsingWos import *
+from .BiblioParsingUtils import *
 from .BiblioParsingGlobals import *
 from .BiblioRefs import *
 from .GUI_utils import *
